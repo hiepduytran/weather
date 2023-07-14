53e39f38294af09a7ae82d2eff1cc4d75a86a18a
Trần Duy Hiệp <116613860+hiepduytran@users.noreply.github.com>
2023-07-14 09:31:03 +0700

    Update Fetching.js

--- src/redux/action/Fetching.js
+++ src/redux/action/Fetching.js
@@ -2,7 +2,7 @@ import { fetchDataSuccess } from "./index";
 
 export const fetchWeather = () => async dispatch => {
   try {
-    const url = "http://api.weatherapi.com/v1/forecast.json?key=f5ac4be4a19c47d8a3e42522222112&q=hanoi&days=10&aqi=no&alerts=yes";
+    const url = "https://api.weatherapi.com/v1/forecast.json?key=f5ac4be4a19c47d8a3e42522222112&q=hanoi&days=10&aqi=no&alerts=yes";
     const response = await fetch(url);
     const responseBody = await response.json();
     
